efb685b52dd4797e425d599a809df16906c6b315
rajat <9462262051rk@gmail.com>
2023-09-15 14:11:50 +0530

    making chats.map now

--- frontend/src/components/Chats/MyChats.jsx
+++ frontend/src/components/Chats/MyChats.jsx
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
 import ChatContext from "../../Context/ChatContext";
-import { useToast } from "@chakra-ui/react";
+import { Box, Button, useToast } from "@chakra-ui/react";
 import axios from "axios";
 import { server } from "../../App";
+import { AddIcon } from "@chakra-ui/icons";
 
 const MyChats = () => {
   const toast = useToast();
@@ -43,7 +44,44 @@ const MyChats = () => {
     // eslint-disable-next-line
   }, []);
 
-  return <div>MyChats</div>;
+  return (
+    <Box
+      display={{ base: selectedChat ? "none" : "flex", md: "flex" }}
+      flexDir={"column"}
+      alignItems={"center"}
+      p={3}
+      bg={"white"}
+      w={{ base: "100%", md: "31%" }}
+      borderRadius={"lg"}
+      borderWidth={"1px"}>
+      <Box
+        pb={3}
+        px={3}
+        fontSize={{ base: "28px", md: "30px" }}
+        fontFamily={"Work sans"}
+        display={"flex"}
+        w={"100%"}
+        justifyContent={"space-between"}
+        alignItems={"center"}>
+        My Chats
+        <Button
+          d="flex"
+          fontSize={{ base: "17px", md: "10px", lg: "17px" }}
+          rightIcon={<AddIcon />}>
+          New Group Chat
+        </Button>
+      </Box>
+      <Box
+        display={"flex"}
+        flexDir={"column"}
+        p={3}
+        bg={"#F8F8F8"}
+        w={"100%"}
+        h={"100%"}
+        borderRadius={"lg"}
+        overflowY={"hidden"}></Box>
+    </Box>
+  );
 };
 
 export default MyChats;
